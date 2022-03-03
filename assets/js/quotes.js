13100b5c4823f4a7537d67cfafb891f5cc5faee2
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2022-03-03 09:07:28 -0500

    Update quotes.js
    Added period. Previous commit fixed mobile issue. MVP completed!

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -5,7 +5,7 @@ const quotes = [
     'You\’re right, all efforts are pointless… If you don\’t believe in yourself.',
     'It\'s true that you\'re different from Neji… You\'re not a genius in ninjutsu nor genjutsu, not even taijutsu… But you know what Lee? You are a genius that has the strength and potential within himself to surpass Neji. Because you are… a genius of hard work.',
     'You have the gift of perseverance, and that\'s what makes you a genius too!',
-    'Hard work and effort will never betray you. Determination will never betray you',
+    'Hard work and effort will never betray you. Determination will never betray you.',
     'Everything Lee has achieved is thanks to his own efforts. The only thing I did as his teacher was believe in him.',
     'THE POWER OF YOUTH!',
 ]
